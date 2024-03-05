6069d203731f1181d2d53e5a34bec02cb4b07725
Juan Carlos Lumbiarres <jclumbiarres@protonmail.com>
2024-03-05 20:08:59 +0100

    Ejemplo validación

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import { Hono } from "hono";
 import { Top } from "./components/Hello";
+import { validator } from "hono/validator";
+import { z } from "zod";
 
 const app = new Hono();
 
@@ -15,5 +17,24 @@ app.get("/salida", (c) => {
   const messages = ["Good Morning", "Good Evening", "Good Night"];
   return c.html(<Top messages={messages} />);
 });
+
+const schema = z.object({
+  nombre: z.string(),
+  edad: z.number(),
+});
+
+app.post(
+  "/post",
+  validator("json", (value, c) => {
+    const parsed = schema.safeParse(value);
+    if (!parsed.success) {
+      return c.json({ message: "Invalid data", errors: parsed.error.errors });
+    }
+    return c.json({ message: "Valid data", data: parsed.data });
+  }),
+  (c) => {
+    return c.json({ message: "Hola Hono!" });
+  }
+);
 
 export default app;
